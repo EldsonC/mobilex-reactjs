729d0e6edf62778a2d160fc88e15d9de02705776
eldson <eldson13@gmail.com>
2022-06-05 21:30:05 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import facebookIcon from './img/facebook-icon.svg'
+import twitterIcon from './img/twitter-icon.svg'
+import googlePlayIcon from './img/google-play-icon.svg'
+import mobilexLogo from './img/MOBILEx-logo.svg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-body">
+      <div className="hiro">
+        <header>
+          <img src={mobilexLogo} alt="MOBILEx-logo"/>
+          <ul>
+              <li>About</li>
+              <li>Our hope</li>
+              <li>Cariers</li>
+              <li>Blog</li>
+              <li>Contact</li>
+          </ul>
+        </header>
+
+        <div className="text-hiro">
+            <span className="title-text-hiro base-font">Join our team</span>
+            <span className="sub-title-hiro">Wich and be yourself</span>
+
+            <button className="button-hiro">
+                <span className="base-font">Open productions</span>
+                <div className="circle-button-hiro">
+                    <span>4</span>
+                </div>
+            </button>
+        </div>
+
+        <footer>
+            <div className="social-medias">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src={twitterIcon} alt="twitter-icon"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src={facebookIcon} alt="facebook-icon"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src={googlePlayIcon} alt="google-play-icon"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="stok">
+                <span>EMC <span className="lyrics-18">18</span></span>
+            </div>
+        </footer>
+      </div>
     </div>
+
   );
 }
 
