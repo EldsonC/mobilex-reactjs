79fded464518f50a6a3ea3807ec49577753fe989
eldson <eldson13@gmail.com>
2022-06-05 22:10:34 -0300

    button component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,12 @@ import facebookIcon from '../assets/img/facebook-icon.svg'
 import twitterIcon from '../assets/img/twitter-icon.svg'
 import googlePlayIcon from '../assets/img/google-play-icon.svg'
 import mobilexLogo from '../assets/img/MOBILEx-logo.svg'
+
+import { ButtonComponent } from './shared/components/button/button';
 import './App.css';
 
 function App() {
-  let styleBorderBottom = 'border-bottom: 3px solid #815664;'
+  
   return (
     <div className="container-body">
       <div className="hiro">
@@ -25,12 +27,7 @@ function App() {
             <span className="title-text-hiro base-font">Join our team</span>
             <span className="sub-title-hiro">Wich and be yourself</span>
 
-            <button className="button-hiro">
-                <span className="base-font">Open productions</span>
-                <div className="circle-button-hiro">
-                    <span>4</span>
-                </div>
-            </button>
+            <ButtonComponent/>
         </div>
 
         <footer>
---
+++ src/app/shared/components/button/button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './button.css'
+export const ButtonComponent = () => {
+    return (
+        <button className="button-hiro">
+            <span className="base-font">Open productions</span>
+            <div className="circle-button-hiro">
+                <span>4</span>
+            </div>
+        </button>
+    )
+}
