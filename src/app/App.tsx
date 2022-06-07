7c3081db520b6faf1b7ed0bcac0d5f41c80b2909
eldson <eldson13@gmail.com>
2022-06-07 14:35:04 -0300

    animation with react springer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useSpring, animated, to } from "react-spring";
+
 import facebookIcon from '../assets/img/facebook-icon.svg'
 import twitterIcon from '../assets/img/twitter-icon.svg'
 import googlePlayIcon from '../assets/img/google-play-icon.svg'
@@ -8,51 +10,81 @@ import { HeaderComponent } from './shared/components/header/header';
 import './App.css';
 
 function App() {
-  
-  return (
-    <div className="container-body">
-        <div className="hiro">
-            <HeaderComponent/>
+    
+    const settingsAnimation = {
+        positions: {
+            setStart: to([-200], (x) => `translateX(${x}px)`),
+            setEnd: to([200], (x) => `translateX(${x}px)`),
+            setBase: to([0], (x) => `translateX(${x}px)`)
+        }
+    }
+    
+    
+    const props = useSpring({
+        from: {
+            opacity: 1,
+            transform: settingsAnimation.positions.setStart,
+        },
+        to: [
+            {
+                opacity: 0,
+                transform: settingsAnimation.positions.setEnd,
+            },
+            {
+                opacity: 1,
+                transform: settingsAnimation.positions.setBase,
+            }
+        ],
+        reset: true,
+        delay: 100
+    })
+    return (
+        <div className="container-body">
+            <div className="hiro">
+                <HeaderComponent/>
 
-            <div className="text-hiro">
-                <span className="title-text-hiro base-font">Join our team</span>
-                <span className="sub-title-hiro">Wich and be yourself</span>
+                <div className="text-hiro">
+                    <animated.div style={{transition: '.1s',...props}}>
+                        <span className="title-text-hiro base-font">Join our team</span>
+                    </animated.div>
+                    
+                    <span className="sub-title-hiro">Wich and be yourself</span>
 
-                <ButtonComponent/>
+                    <ButtonComponent/>
+                </div>
+
+                <footer>
+                    <div className="social-medias">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src={twitterIcon} alt="twitter-icon"/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src={facebookIcon} alt="facebook-icon"/>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src={googlePlayIcon} alt="google-play-icon"/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="stok">
+                        <span>EMC <span className="lyrics-18">18</span></span>
+                    </div>
+                </footer>
             </div>
 
-            <footer>
-                <div className="social-medias">
-                    <ul>
-                        <li>
-                            <a href="#">
-                                <img src={twitterIcon} alt="twitter-icon"/>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <img src={facebookIcon} alt="facebook-icon"/>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <img src={googlePlayIcon} alt="google-play-icon"/>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="stok">
-                    <span>EMC <span className="lyrics-18">18</span></span>
-                </div>
-            </footer>
+            <div className='main'>
+
+            </div>
         </div>
 
-        <div className='main'>
-
-        </div>
-    </div>
-
-  );
+    );
 }
 
 export default App;
--- src/app/shared/components/button/button.tsx
+++ src/app/shared/components/button/button.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
 
 import './button.css'
 export const ButtonComponent = () => {
+    const [flip, set] = useState(false);
+    const props = useSpring({
+        from: {opacity: 0},
+        to: {opacity: 1},
+        reset: true,
+        reverse: flip,
+        delay: 300,
+        onRest: () => set(!flip)
+    })
     return (
+        
         <button className="button-hiro">
             <span className="base-font">Open productions</span>
             <div className="circle-button-hiro">
                 <span>4</span>
             </div>
-        </button>
+        </button> 
+        
+        
     )
 }
