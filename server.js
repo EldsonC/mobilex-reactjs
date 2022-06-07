bf29d121f15cb9320f53958be5872d3d9031fced
eldson <eldson13@gmail.com>
2022-06-07 10:26:52 -0300

    server route

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const { resolve } = require('path')
+const  path  = require('path')
 
 app.use(cors())
 app.use(express.static(
-    resolve(
-        __dirname,
-        './build'
-    )
+    __dirname + "./build"
 ))
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + "./build/index.html"))
+})
 
 app.listen(process.env.PORT || 3000)
